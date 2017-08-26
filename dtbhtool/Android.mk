aa963f774b356e1c9e36ca5d4e8d190182e54a02
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-08-26 17:56:41 +0200

    libfdt: drop local lib in favour of most updated AOSP libfdt

--- dtbhtool/Android.mk
+++ dtbhtool/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := dtbimg.c
 LOCAL_STATIC_LIBRARIES := libfdt
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/libfdt
+LOCAL_C_INCLUDES := external/dtc/libfdt
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/libdtbimg
 
 LOCAL_MODULE := libdtbimg
@@ -56,5 +56,3 @@ LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
 
 $(call dist-for-goals,dist_files,$(LOCAL_BUILT_MODULE))
-
-include $(LOCAL_PATH)/libfdt/Android.mk
